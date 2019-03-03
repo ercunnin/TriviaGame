f2832ee774e9f100b63aebcaaba0ec4bb92c8777
The Dude <thedude@Erics-MacBook-Pro.local>
2019-03-03 10:43:03 -0700

    js1

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    // event listeners
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click', '.option', trivia.guessChecker);
+})
