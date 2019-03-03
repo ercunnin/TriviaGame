d6082ec2dd1ea5b719b226956fbf8b5f8ef9d3be
The Dude <thedude@Erics-MacBook-Pro.local>
2019-03-03 10:52:19 -0700

    js2

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 $(document).ready(function () {
 
-    // event listeners
     $("#remaining-time").hide();
     $("#start").on('click', trivia.startGame);
     $(document).on('click', '.option', trivia.guessChecker);
 })
+var trivia = {
+
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId: '',
+    questions: {
+        q1: 'What type of farm does Dwight own?',
+        q2: '',
+        q3: '',
+        q4: '',
+        q5: "",
+        q6: '',
+        q7: "",
+        q8: ''
+    },
+        options: {
+            q1: 
+            q2:
+            q4:
+            q4:
+            q5:
+            q6:
+            q7:
+            q8:
+        }
+        answers: {
+            q1:
+            q2: 
+            q3: 
+            q4: 
+            q5: 
+            q6: 
+            q7: 
+          },
